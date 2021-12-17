b705b6d72b2ad1fb84b648294fae813e55f26095
Shubham Seth <shubhamsth1234@gmail.com>
2021-12-17 23:29:09 +0530

    Added FlipCoin and LeapYear problem

---
+++ FlipCoin1/FlipCoin1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FlipCoin1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int head = 0;
+            int tail = 0;
+
+            Random rand = new Random();
+            Console.WriteLine("Enter the total number of tosses you want: ");
+            int userInput = Convert.ToInt32(Console.ReadLine()); // To convert string into integer
+
+            int i = 0;
+            while ( i < userInput )
+            {
+                int myKey = rand.Next(0, 2);
+                if (myKey == 0)
+                {
+                    head++;
+                }
+                else
+                {
+                    tail++;
+                }
+                i++;
+            }
+
+            double resultHead = (head * 100) / userInput;
+            double resultTail = (tail * 100) / userInput;
+
+            Console.WriteLine("Percentage of Head  = " + resultHead + "%");
+            Console.WriteLine("Percentage of Tail = " + resultTail + "%");
+        }
+    }
+}
---
+++ LeapYear2/LeapYear2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeapYear2
+{
+    class Program
+    {
+        static bool checkYear(int year)
+        {
+            // Return true if year is a multiple
+            // of 4 and not multiple of 100.
+            // OR year is multiple of 400.
+            return (((year % 4 == 0) && (year % 100 != 0)) ||
+                    (year % 400 == 0));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the 4 digit year you want to check:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write(checkYear(year) ?year + " is Leap Year" 
+                : year + " is not a Leap Year"); 
+            /*
+            Ternary operator --> ( expression ? a : b)
+            if expression --> true then print 'a' 
+                                   else print 'b'
+             */
+        }
+    }
+}
